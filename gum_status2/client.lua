42f964c184df06f79561b5b772efc4424cdaf6b0
Gummik <69731717+gummyantifi@users.noreply.github.com>
2022-06-27 16:29:58 +0200

    Alcohol stats, voice stat, temp stat, minimap setup

--- gum_status2/client.lua
+++ gum_status2/client.lua
@@ -1,5 +1,10 @@
 local hud = false
 local compass = false
+local voiceVolume = 25
+local voiceOn = false
+
+local YouNeedMinimap = false
+
 RegisterCommand("hud", function(source, args, rawCommand)
 	if hud == true then
         hud = false
@@ -7,6 +12,7 @@ RegisterCommand("hud", function(source, args, rawCommand)
         hud = true
 	end
 end)
+
 RegisterCommand("prog_test", function(source, args, rawCommand)
     exports.gum_status2:DisplayProgressBar(15000)
 end)
@@ -33,12 +39,34 @@ AddEventHandler('gum_status:enable_compass', function()
     end
 end)
 
--- RegisterNetEvent("gum:SelectedCharacter")
--- AddEventHandler("gum:SelectedCharacter", function(charid)
+RegisterNetEvent("gum:SelectedCharacter")
+AddEventHandler("gum:SelectedCharacter", function(charid)
     Citizen.CreateThread(function()
         hud = true
     end)
--- end)
+end)
+
+RegisterNetEvent('gum_status:voiceOn')
+AddEventHandler('gum_status:voiceOn', function()
+    if voiceOn == false then
+        voiceOn = true
+    else
+        voiceOn = false
+    end
+end)
+
+RegisterNetEvent('gum_status:range')
+AddEventHandler('gum_status:range', function(range)
+    if tonumber(range) == 30 then
+        voiceVolume = 100
+    elseif tonumber(range) == 15 then
+        voiceVolume = 75
+    elseif tonumber(range) == 8 then
+        voiceVolume = 50
+    elseif tonumber(range) == 3.5 then
+        voiceVolume = 25
+    end
+end)
 
 Citizen.CreateThread(function()
     while true do
@@ -51,10 +79,13 @@ Citizen.CreateThread(function()
         else
             SendNUIMessage({compass=false})
         end
+        
         Citizen.InvokeNative(0x4CC5F2FC1332577F, GetHashKey("HUD_CTX_INFINITE_AMMO"))
-        Citizen.InvokeNative(0x4CC5F2FC1332577F, GetHashKey("HUD_CTX_SHARP_SHOOTER_EVENT"))
-        Citizen.InvokeNative(0x4CC5F2FC1332577F ,GetHashKey("HUD_CTX_RESTING_BY_FIRE"))
-        Citizen.InvokeNative(0x4CC5F2FC1332577F ,GetHashKey("HUD_CTX_INFO_CARD"))
+        if YouNeedMinimap == false then
+            Citizen.InvokeNative(0x4CC5F2FC1332577F, GetHashKey("HUD_CTX_SHARP_SHOOTER_EVENT"))
+            Citizen.InvokeNative(0x4CC5F2FC1332577F ,GetHashKey("HUD_CTX_RESTING_BY_FIRE"))
+            Citizen.InvokeNative(0x4CC5F2FC1332577F ,GetHashKey("HUD_CTX_INFO_CARD"))
+        end
         Citizen.InvokeNative(0x4CC5F2FC1332577F ,GetHashKey("HUD_CTX_GOLD_CURRENCY_CHANGE"))
 
         Citizen.InvokeNative(0xD4EE21B7CC7FD350, false)
@@ -70,6 +101,8 @@ Citizen.CreateThread(function()
         local mystamina = Citizen.InvokeNative(0x36731AC041289BB1, PlayerPedId(), 1)
         local myhunger = exports["gum_metabolism"]:getHunger()
         local mythirst = exports["gum_metabolism"]:getThirst()
+        local myalcohol = exports["gum_metabolism"]:getAlcohol()
+        local mytemp = exports["gum_metabolism"]:getTemp()
         local horsestamina = Citizen.InvokeNative(0x36731AC041289BB1, GetMount(PlayerPedId()), 1)
         local horsehealth = Citizen.InvokeNative(0x36731AC041289BB1, GetMount(PlayerPedId()), 0)
         if myhealth == false then
@@ -80,12 +113,12 @@ Citizen.CreateThread(function()
         end
         if hud == true then
             if IsCinematicCamRendering() == 1 then
-                SendNUIMessage({show=false, health=myhealth, thirst=math.floor(mythirst), hunger=math.floor(myhunger), stamina=mystamina, on_horse=false, horse_stamina=horsestamina, horse_health=horsehealth })
+                SendNUIMessage({show=false, health=myhealth, thirst=math.floor(mythirst), hunger=math.floor(myhunger), stamina=mystamina, on_horse=false, horse_stamina=horsestamina, horse_health=horsehealth, alcohol=myalcohol, temp=(2.5*mytemp),voice=voiceOn,volume=voiceVolume,minimap=YouNeedMinimap })
             else
-                SendNUIMessage({show=true, health=myhealth, thirst=math.floor(mythirst), hunger=math.floor(myhunger), stamina=mystamina, on_horse=onhorse, horse_stamina=horsestamina, horse_health=horsehealth })
+                SendNUIMessage({show=true, health=myhealth, thirst=math.floor(mythirst), hunger=math.floor(myhunger), stamina=mystamina, on_horse=onhorse, horse_stamina=horsestamina, horse_health=horsehealth, alcohol=myalcohol, temp=(2.5*mytemp),voice=voiceOn,volume=voiceVolume,minimap=YouNeedMinimap })
             end
         else
-            SendNUIMessage({show=false, health=myhealth, thirst=math.floor(mythirst), hunger=math.floor(myhunger), stamina=mystamina, on_horse=false, horse_stamina=horsestamina, horse_health=horsehealth })
+            SendNUIMessage({show=false, health=myhealth, thirst=math.floor(mythirst), hunger=math.floor(myhunger), stamina=mystamina, on_horse=false, horse_stamina=horsestamina, horse_health=horsehealth, alcohol=myalcohol, temp=(2.5*mytemp),voice=voiceOn,volume=voiceVolume,minimap=YouNeedMinimap })
         end
     end
 end)
